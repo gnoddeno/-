c7aad17a7cedded035c92717576bd00f00e74306
항동홍 <hangdonghong@hangdonghong-ui-MacBookAir.local>
2024-08-02 20:51:08 +0900

    springboot database에 데이터 추가

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainContriller {
+public class MainController {
     @GetMapping("/demo")
     @ResponseBody
     public String index(){
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa(){
+		Question q1 = new Question();
+		q1.setSubject("Question 1");
+		q1.setContent("Content 1");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("Question 2");
+		q2.setContent("Content 2");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
 	}
 
 }
