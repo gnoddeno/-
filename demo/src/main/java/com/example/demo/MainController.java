727d1655a1c9895ec7d7a09fab37b4a49a17de8d
gnoddeno <gnoddeno@naver.com>
2024-08-06 21:48:39 +0900

    🌱 <Springboot> : Setting Root URL

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -11,4 +11,10 @@ public class MainController {
     public String index(){
         return "Hello World";   
     }
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
+    }
+
 }
