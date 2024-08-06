bb033717ec81b9a0f0e4ea8a53dbb38c0fa8be5e
gnoddeno <gnoddeno@naver.com>
2024-08-06 22:04:08 +0900

    🌱 <SpringBoot> : Add detail content page

--- demo/src/main/java/com/example/demo/question/QuestionService.java
+++ demo/src/main/java/com/example/demo/question/QuestionService.java
@@ -1,8 +1,11 @@
 package com.example.demo.question;
 
 import java.util.List;
+
+import com.example.demo.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,5 +14,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }else{
+            throw new DataNotFoundException("Question not found");
+        }
     }
 }
