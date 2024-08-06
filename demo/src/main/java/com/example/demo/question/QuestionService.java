622af62d01c4b275651a0b7dfa162eec0f9caf1f
gnoddeno <gnoddeno@naver.com>
2024-08-06 21:52:26 +0900

    🌱 <Springboot> : Add func Service

---
+++ demo/src/main/java/com/example/demo/question/QuestionService.java
@@ -0,0 +1,15 @@
+package com.example.demo.question;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return this.questionRepository.findAll();
+    }
+}
